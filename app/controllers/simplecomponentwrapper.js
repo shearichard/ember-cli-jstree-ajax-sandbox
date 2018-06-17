15df03dbd61e5e38b47d6e55721bf069d745081a
Richard Shea <rshea@thecubagroup.com>
2018-06-17 17:04:55 +1200

    After creation of simplecomponentwrapper controller and making use of the properties on that controller to invoke the wrapper component

---
+++ app/controllers/simplecomponentwrapper.js
@@ -0,0 +1,8 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  firstTitle:"First from component 0 (Wrapper)",
+  secondTitle:"Second from component (Wrapper)",
+  firstNumber:2001,
+  secondNumber:2011
+});
