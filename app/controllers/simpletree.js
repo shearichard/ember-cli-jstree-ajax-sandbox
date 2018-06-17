04563da296edfeba8d001c3c4be1b6eb5549a35e
Richard Shea <rshea@thecubagroup.com>
2018-06-17 17:52:12 +1200

    Working ember-cli-jstree

--- app/controllers/simpletree.js
+++ app/controllers/simpletree.js
@@ -1,4 +1,32 @@
 import Controller from '@ember/controller';
+import EmberObject, { computed } from '@ember/object';
 
 export default Controller.extend({
+  data: computed(() => [
+    "Simple root node",
+    {
+      text: "Single child node (has tooltip)",
+      type: "single-child",
+      children: ["one child"],
+      a_attr: { class: "hint--top", "data-hint": "Use a_attr to add tooltips" }
+    },
+    {
+      id: "rn2",
+      text: "Opened node (has tooltip)",
+      state: {
+        opened: true,
+        selected: true
+      },
+      a_attr: {
+        class: "hint--bottom",
+        "data-hint": "This is a bottom mounted node tooltip"
+      },
+      children: [
+        {
+          text: "Child 1"
+        },
+        "Child 2"
+      ]
+    }
+]),
 });
